0e442596549f46be5a66bbee063a97166e591bce
韧 <15676585630@163.com>
2022-05-23 01:38:39 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 window.__PRE_CONFIG__ = {
-  header: 'Server Status',
-  subHeader: 'Servers\' Probes Set up with ServerStatus',
+  header: '沐深的监控小站',
+  subHeader: '看看今天小鸡炸了几个？',
   interval: 1.5,
   footer: '<p>Powered by <a href="https://github.com/cokemine/ServerStatus-Hotaru">ServerStatus-Hotaru</a></p>'
 };
